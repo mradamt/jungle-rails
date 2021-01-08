c91cea4a43b4d79f428ce064220ca531cec116f7
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-07 21:12:35 -0800

    add 'sales' link to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,5 +5,6 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count
     @category_count = Category.count
+    @sale_count = Sale.count
   end
 end
