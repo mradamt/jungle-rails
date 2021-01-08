0354ca871c0b1d0131c9a6edd9501d78563ff3d7
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-07 16:44:21 -0800

    complete working version w/o logout: add sessions save functionality

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  before_filter :authorize
+
   def show
     @product_count = Product.count
     @category_count = Category.count
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
@@ -21,9 +21,4 @@ class SessionsController < ApplicationController
     redirect_to '/login'
   end
   
-  private
-
-  def session_params
-    params.require(:session).permit(:name, :email, :password, :password_confirmation)
-  end
 end
