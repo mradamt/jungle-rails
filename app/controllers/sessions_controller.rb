9b9b2767900304689e212a2a79887d76efdeea05
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-07 17:00:54 -0800

    refactor sessions/new, use form_for (so css is consistent)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_email(session_params[:email])
     # If the user exists AND the password entered is correct.
-    if @user && @user.authenticate(params[:password])
+    if @user && @user.authenticate(session_params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = @user.id
@@ -19,6 +19,12 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
   
 end
