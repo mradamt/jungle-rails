9e49fb52f9a395d5d36c4af0cea9d65fc756de22
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-07 17:38:58 -0800

    bugfix: add on delete cascade to allow products to be deleted from admin/products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ActiveRecord::Base
 
   belongs_to :category
 
+  has_many :line_items, dependent: :destroy
+
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
