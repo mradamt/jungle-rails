0c15290b5a0a9d84a03010d9627bb2f64c74204b
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-11 15:14:37 -0800

    set up initial test "new product saves successfully"

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,5 +3,24 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
 
+    before do
+      @category = Category.new(name: 'valid')
+      @category.save
+      @product = Product.new(
+        name: 'horse',
+        price: 100,
+        quantity: 5,
+        category_id: @category.id
+      )
+      @product.save
+      # byebug
+    end
+    
+    it "new product saves successfully" do
+      expect(Product.last).to eq(@product)
+    end
+
+    
+
   end
 end
