8291029ac183f6562ee301c0cad014da55b67423
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-12 16:14:02 -0800

    update section comments

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -21,9 +21,10 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
 
-    # DEBUG / VERIFY
+    # DEBUG
     # save_screenshot
 
+    # VERIFY
     expect(page).to have_css 'article.product', count: 10
 
   end
