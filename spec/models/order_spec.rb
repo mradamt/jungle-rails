30d6f0e1ca9d1d7d7ebc2522d02f667e74a2a77f
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-12 18:27:35 -0800

    cut down lines of code

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -15,32 +15,22 @@ RSpec.describe Order, type: :model do
       # 1. initialize order with necessary fields (see orders_controllers, schema and model definition for what is required)
       @order = Order.new(email: 'test@test.com', total_cents: 100, stripe_charge_id: 'test')
       # 2. build line items on @order
-      # [@product1, @product2].each do |entry|
-      #   quantity = entry[:quantity]
-      #   price = entry[:price_cents] / 100
-      #   @order.line_items.new(
-      #     product: entry,
-      #     quantity: 5,
-      #     item_price: price,
-      #     total_price: price * quantity
-      #   )
-      # end
-      @order.line_items.new(product: @product1, quantity: 5, item_price: 1, total_price: 1)
-      @order.line_items.new(product: @product2, quantity: 5, item_price: 1, total_price: 1)
+      @order.line_items.new(product: @product1, quantity: 2, item_price: 1, total_price: 1)
+      @order.line_items.new(product: @product2, quantity: 2, item_price: 1, total_price: 1)
       # 3. save! the order - ie raise an exception if it fails (not expected)
       @order.save!
       # 4. reload products to have their updated quantities
       @product1.reload
       @product2.reload
       # 5. use RSpec expect syntax to assert their new quantity values
-      expect(@product1[:quantity]).to eq(5)   # -1
-      expect(@product2[:quantity]).to eq(0)   # -1
+      expect(@product1[:quantity]).to eq(8)   # -2
+      expect(@product2[:quantity]).to eq(3)   # -2
       expect(@product3[:quantity]).to eq(2)   # unchanged
     end
     
-    it 'does not deduct quantity from products that are not in the order' do
-      # TODO: Implement based on hints in previous test
-      # byebug
-    end
+    # it 'does not deduct quantity from products that are not in the order' do
+    #   # TODO: Implement based on hints in previous test
+    #   # byebug
+    # end
   end
 end
