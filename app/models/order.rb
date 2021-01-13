5fff21fbe670055b76a8a715a98c31d53592ede5
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-13 11:21:16 -0800

    write inventory-update method that passes test - clumsy version

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,21 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  after_create :update_inventory
+
+  private
+
+  def update_inventory
+    order = LineItem.where(order_id: self.id)
+    order.each do |lineItem|
+      product_id = lineItem[:product_id]
+      @product = Product.find(product_id)
+      quantity = @product[:quantity]
+      delta = quantity - lineItem[:quantity]
+      
+      @product.update(quantity: delta)
+      byebug
+    end
+  end
+
 end
